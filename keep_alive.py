fe5b4c1f436b58a23229b94d799425133b121264
depor0mk-cloud <depor0mk@gmail.com>
2026-02-18 23:41:19 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,8 @@
 from flask import Flask
 from threading import Thread
-
 app = Flask('')
-
 @app.route('/')
 @app.route('/health')
-def home():
-    return "Бот живой!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+def home(): return "✅ Бот работает!"
+def run(): app.run(host='0.0.0.0', port=8080)
+def keep_alive(): Thread(target=run).start()
